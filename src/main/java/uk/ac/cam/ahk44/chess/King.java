cdd1c34399661e2b19af0bfa03a15e95c652ac2d
amirhkk <amir.hkkh@gmail.com>
2022-01-17 19:49:48 +0000

    ?

--- src/main/java/uk/ac/cam/ahk44/chess/King.java
+++ src/main/java/uk/ac/cam/ahk44/chess/King.java
@@ -6,6 +6,8 @@ import java.util.List;
 @Component
 public class King extends Piece{
 
+    static int KING_VALUE = 10000;
+
     public King(char name, Position piecePosition, PieceColor pieceColor, Board board) {
         super(name, piecePosition, pieceColor, board);
         this.name = name;
